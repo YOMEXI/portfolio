dff6da6937c903004b22b4036f8d32a667249500
YOMEXI <abayomexy@gmail.com>
2022-05-01 06:09:44 +0100

    updated cv

--- pages/projects/Projects.tsx
+++ pages/projects/Projects.tsx
@@ -6,15 +6,15 @@ const Projects = () => {
     <>
       <div className="  sm:flex sm:flex-col w-full sm:justify-center sm:items-center block text-center mb-5">
         <h3 className="text-lg sm:text-xl font-bold my-2 text-center">
-          TechBlog
+          TechBlog (Fullstack)
         </h3>
         <p className="text-sm sm:text-lg my-1 text-center">
           A Tech Blog application built using{" "}
           <span className="font-medium ">reactjs</span>,{" "}
           <span className="font-medium">redux</span>,
-          <span className="font-medium">Nodejs</span>,
-          <span className="font-medium">Expressjs</span>,
-          <span className="font-medium">MongoDb</span>
+          <span className="font-medium">Material Ui</span>,
+          <span className="font-medium">Nestjs</span>,
+          <span className="font-medium">Postgresql</span>
         </p>
         <a
           href="https://techblog-client.herokuapp.com
@@ -53,6 +53,47 @@ const Projects = () => {
             <SiGithub />
           </span>{" "}
           client: https://github.com/YOMEXI/Techblog-client
+        </a>
+      </div>
+
+      <div className="  sm:flex sm:flex-col w-full sm:justify-center sm:items-center block text-center mb-5">
+        <h3 className="text-lg sm:text-xl font-bold my-2 text-center">
+          Realtyr (Frontend)
+        </h3>
+        <p className="text-sm sm:text-lg my-1 text-center">
+          A Real estate application built using
+          <span className="font-medium ">reactjs</span>,{" "}
+          <span className="font-medium">redux</span>,
+          <span className="font-medium">Chakra Ui</span>,
+        </p>
+        <a
+          href=" https://realtyr.herokuapp.com/
+       "
+        >
+          {" "}
+          <img
+            src="/img/realty.png"
+            alt=""
+            className=" sm:w-2/5 sm:h-2/5 px-4   w-4/5 m-auto cursor-pointer"
+          />
+        </a>
+        <a
+          className="cursor-pointer mt-3 flex justify-center items-center text-sm "
+          href="https://realtyr.herokuapp.com/"
+        >
+          <span className="mr-2">
+            <i className="fa fa-globe"></i>
+          </span>{" "}
+          https://realtyr.herokuapp.com/
+        </a>
+        <a
+          className="cursor-pointer mt-3 flex justify-center items-center text-sm "
+          href="https://github.com/YOMEXI/Realtyr"
+        >
+          <span className="mr-2">
+            <SiGithub />
+          </span>{" "}
+          api:https://github.com/YOMEXI/Realtyr
         </a>
       </div>
     </>
