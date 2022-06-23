091a76f4fd4c3c01c2dca20e67bb29db83111b44
YOMEXI <abayomexy@gmail.com>
2022-06-23 19:04:38 +0100

    v2

--- pages/projects/Projects.tsx
+++ pages/projects/Projects.tsx
@@ -58,42 +58,53 @@ const Projects = () => {
 
       <div className="  sm:flex sm:flex-col w-full sm:justify-center sm:items-center block text-center mb-5">
         <h3 className="text-lg sm:text-xl font-bold my-2 text-center">
-          Realtyr (Frontend)
+          Findeed (Fullstack)
         </h3>
         <p className="text-sm sm:text-lg my-1 text-center">
-          A Real estate application built using
+          An mini Indeed application built using <br />
           <span className="font-medium ">reactjs</span>,{" "}
-          <span className="font-medium">redux</span>,
-          <span className="font-medium">Chakra Ui</span>,
+          <span className="font-medium">Redux</span> ,
+          <span className="font-medium">Nodejs</span> ,
+          <span className="font-medium">Nestjs</span> ,
+          <span className="font-medium">Postgresql</span>
         </p>
         <a
-          href=" https://realtyr.herokuapp.com/
+          href="https://findeed-client.herokuapp.com
        "
         >
           {" "}
           <img
-            src="/img/realty.png"
+            src="/img/findeed.png"
             alt=""
             className=" sm:w-2/5 sm:h-2/5 px-4   w-4/5 m-auto cursor-pointer"
           />
         </a>
         <a
           className="cursor-pointer mt-3 flex justify-center items-center text-sm "
-          href="https://realtyr.herokuapp.com/"
+          href="https://findeed-client.herokuapp.com"
         >
           <span className="mr-2">
             <i className="fa fa-globe"></i>
           </span>{" "}
-          https://realtyr.herokuapp.com/
+          https://findeed-client.herokuapp.com
         </a>
         <a
           className="cursor-pointer mt-3 flex justify-center items-center text-sm "
-          href="https://github.com/YOMEXI/Realtyr"
+          href="https://github.com/YOMEXI/indeed-clone-api"
         >
           <span className="mr-2">
             <SiGithub />
           </span>{" "}
-          api:https://github.com/YOMEXI/Realtyr
+          api:https://github.com/YOMEXI/indeed-clone-api
+        </a>
+        <a
+          className="cursor-pointer mt-3 flex justify-center items-center text-sm "
+          href="https://github.com/YOMEXI/indeed-clone-client"
+        >
+          <span className="mr-2">
+            <SiGithub />
+          </span>{" "}
+          client:https://github.com/YOMEXI/indeed-clone-client
         </a>
       </div>
     </>
